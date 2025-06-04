64b9fb79342012a14edd0ac912f3a9917ef8b1ef
Dmitry Off <36734090+dsmirnoff73@users.noreply.github.com>
2025-06-04 16:04:26 +0200

    changed port 8080

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("web_server:app", host="0.0.0.0", port=port)
