72e84484c942ca9c2084a807261906985ad8fdb6
Dmitry Off <36734090+dsmirnoff73@users.noreply.github.com>
2025-06-04 15:10:32 +0200

    добавлено окружение с гугл джесон

--- sheets_writer.py
+++ sheets_writer.py
@@ -1,13 +1,15 @@
 import os
 import gspread
+import json
 from google.oauth2.service_account import Credentials
 
 SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME")
 SHEET_NAME = os.getenv("SHEET_NAME", "08_Прогресс")
 
 def write_to_sheets(entry):
-    creds_path = os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
-    creds = Credentials.from_service_account_file(creds_path, scopes=["https://www.googleapis.com/auth/spreadsheets"])
+    creds_json = os.getenv("GOOGLE_CREDS_JSON")
+    creds_dict = json.loads(creds_json)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=["https://www.googleapis.com/auth/spreadsheets"])
     client = gspread.authorize(creds)
     sheet = client.open_by_key(SPREADSHEET_NAME).worksheet(SHEET_NAME)
     sheet.append_row([
